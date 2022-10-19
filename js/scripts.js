07f68df77857878ec8bf96e74c67252ab3470e9d
Tadit Dash <taditdash@gmail.com>
2022-10-19 14:14:30 +0530

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Freelancer v7.0.4 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2021 Start Bootstrap
+* Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
 */
 //
@@ -50,4 +50,50 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+
+    gql(GET_USER_ARTICLES, { page: 0 })
+    .then(result => {
+        const articles = result.data.user.publication.posts;
+        let recentBlogs = document.getElementById('recent-blogs');
+
+        articles.forEach(article => {
+            let blogAnchor = document.createElement('a');
+            blogAnchor.innerText = article.title;
+            blogAnchor.href = `https://blog.taditdash.com/${article.slug}`;
+            blogAnchor.target = '_blank';
+            blogAnchor.classList = 'list-group-item list-group-item-action';
+
+            recentBlogs.appendChild(blogAnchor);
+        })
+    });
 });
+
+async function gql(query, variables={}) {
+    const data = await fetch('https://api.hashnode.com/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'b2d3a70a-50e8-462c-8027-cbed2e1ea7ef'
+        },
+        body: JSON.stringify({
+            query,
+            variables
+        })
+    });
+
+    return data.json();
+}
+
+const GET_USER_ARTICLES = `
+    query GetUserArticles($page: Int!) {
+        user(username: "taditdash") {
+            publication {
+                posts(page: $page) {
+                    title
+                    brief
+                    slug
+                }
+            }
+        }
+    }
+`;
